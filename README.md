d25d261211223db4dbeafdac4d682561511b9684
tamzi <tamrefrank@gmail.com>
2018-08-07 02:52:53 +0300

    added main,js functionality

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # maaextension
 A brackets extension thats saves your time by helping you quickly insert a html file
+ with prefilled HTML code.
---
+++ main.js
@@ -0,0 +1,48 @@
+define(function (require, exports, module) {
+
+    	"use strict";
+
+	var docIndex = 1,
+	    commandId = "new-htmldoc",
+	    menuID = "jt.menuID",
+	    menuLabel = "New HTML file",
+		DocumentManager = brackets.getModule("document/DocumentManager"),
+		Commands = brackets.getModule("command/Commands"),
+		CommandManager = brackets.getModule("command/CommandManager"),
+		KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
+		EditorManager = brackets.getModule("editor/EditorManager"),
+		MainViewManager = brackets.getModule("view/MainViewManager"),
+		Menus = brackets.getModule("command/Menus"),
+		bootstrapTemplate = require("text!template/start.html"),
+		toolbar = $("#main-toolbar"),
+		menu;
+
+
+	function templateHandle(templateContent) {
+		try {
+			var activeEditor = EditorManager.getActiveEditor();
+			activeEditor.document.replaceRange(templateContent, activeEditor.getCursorPos());
+		} catch (err) {}
+	}
+
+	function newFileHandle() {
+		var defaultExtension = ".html",
+		    doc = DocumentManager.createUntitledDocument(docIndex++, defaultExtension);
+
+		MainViewManager._edit(MainViewManager.ACTIVE_PANE, doc);
+		templateHandle(htmlTemplate);
+		return new $.Deferred().resolve(doc).promise();
+	}
+
+	toolbar.on('dblclick', function (e) {
+		if (e.target === this) {
+			newFileHandle();
+		}
+	});
+
+	CommandManager.register(menuLabel, menuID, newFileHandle);
+	menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+	menu.addMenuItem(menuID, undefined, Menus.AFTER, Commands.FILE_NEW_UNTITLED);
+    KeyBindingManager.addBinding(menuID, "Ctrl-Alt-H", "mac");
+	KeyBindingManager.addBinding(menuID, "Ctrl-Alt-H", "win");
+});
